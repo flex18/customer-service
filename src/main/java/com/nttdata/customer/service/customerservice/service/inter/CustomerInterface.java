a33d86023e2024e6f26423527e58f3939f25b927
flex18 <emendozaminchan@gmail.com>
2024-03-05 11:16:27 -0500

    delete import

--- src/main/java/com/nttdata/customer/service/customerservice/service/inter/CustomerInterface.java
+++ src/main/java/com/nttdata/customer/service/customerservice/service/inter/CustomerInterface.java
@@ -2,7 +2,6 @@ package com.nttdata.customer.service.customerservice.service.inter;
 
 import com.nttdata.customer.service.customerservice.entity.CustomerRequest;
 import com.nttdata.customer.service.customerservice.entity.CustomerResponse;
-import java.util.List;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
